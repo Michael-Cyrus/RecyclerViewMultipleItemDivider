a8e1d8820e340f8cc9c3faf0e8b752d0f2b4f80c
chenyao <michael_cyrus@163.com>
2017-07-24 19:33:41 +0800

    recyclerView 多种item 网格布局实现分割线

--- app/src/main/java/com/michael/recyclerview/DividerGridItemDecoration.java
+++ app/src/main/java/com/michael/recyclerview/DividerGridItemDecoration.java
@@ -179,16 +179,16 @@ public class DividerGridItemDecoration extends RecyclerView.ItemDecoration {
         Log.e("getItemOffsets", "过时的 getItemOffsets ");
         int spanCount = getSpanCount(parent, itemPosition);
         int childCount = parent.getAdapter().getItemCount();
-//        if (isLastRaw(parent, itemPosition, spanCount, childCount)) {   // 如果是最后一行，则不需要绘制底部
-//            outRect.set(0, 0, mDivider.getIntrinsicWidth(), 0);
-//        } else if (isLastColum(parent, itemPosition, spanCount, childCount)) {  // 如果是最后一列，则不需要绘制右边
-//            Log.e("getItemOffsets", "mDivider.getIntrinsicHeight() : "+ mDivider.getIntrinsicHeight());
-//            outRect.set(0, 0, 0, mDivider.getIntrinsicHeight());
-//        } else {
-//            Log.e("getItemOffsets", "mDivider.getIntrinsicWidth() : "+ mDivider.getIntrinsicWidth());
-//            outRect.set(0, 0, mDivider.getIntrinsicWidth(), mDivider.getIntrinsicHeight());
-//        }
-        outRect.set(0, 0, mDivider.getIntrinsicWidth(), mDivider.getIntrinsicHeight());
+        if (isLastRaw(parent, itemPosition, spanCount, childCount)) {   // 如果是最后一行，则不需要绘制底部
+            outRect.set(0, 0, mDivider.getIntrinsicWidth(), 0);
+        } else if (isLastColum(parent, itemPosition, spanCount, childCount)) {  // 如果是最后一列，则不需要绘制右边
+            Log.e("getItemOffsets", "mDivider.getIntrinsicHeight() : "+ mDivider.getIntrinsicHeight());
+            outRect.set(0, 0, 0, mDivider.getIntrinsicHeight());
+        } else {
+            Log.e("getItemOffsets", "mDivider.getIntrinsicWidth() : "+ mDivider.getIntrinsicWidth());
+            outRect.set(0, 0, mDivider.getIntrinsicWidth(), mDivider.getIntrinsicHeight());
+        }
+//        outRect.set(0, 0, mDivider.getIntrinsicWidth(), mDivider.getIntrinsicHeight());
     }
 
     @Override
